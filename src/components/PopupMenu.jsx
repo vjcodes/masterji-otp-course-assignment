1ead164ab7504735fd294dac8ede77f2286138a5
vjcodes <47989148+vjcodes@users.noreply.github.com>
2024-07-26 00:56:54 +0530

    CA- Added icons in popup

--- src/components/PopupMenu.jsx
+++ src/components/PopupMenu.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import optionsImg from "./../../public/static/images/optionsImg.svg";
+import deleteImg from "./../../public/static/images/deleteImg.svg";
+import upArrow from "./../../public/static/images/upArrow.svg";
+import downArrow from "./../../public/static/images/downArrow.svg";
 
-const PopupMenu = ({removeItem, moveToTop, moveToBottom, courseIndex}) => {
+const PopupMenu = ({ removeItem, moveToTop, moveToBottom, courseIndex }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -14,23 +17,26 @@ const PopupMenu = ({removeItem, moveToTop, moveToBottom, courseIndex}) => {
           <div className="py-1">
             <a
               href="#"
-              className="block px-4 py-2 text-sm bg-white text-gray-700 hover:bg-gray-100"
+              className="block px-4 py-2 text-sm bg-white text-gray-700 hover:bg-gray-100 flex items-center"
               onMouseDown={() => moveToTop(courseIndex)}
             >
+              <img src={upArrow} alt="moveUp" className="mr-2" />
               Move To Top
             </a>
             <a
               href="#"
-              className="block px-4 py-2 bg-white text-sm text-gray-700 hover:bg-gray-100"
+              className="block px-4 py-2 bg-white text-sm text-gray-700 hover:bg-gray-100 flex items-center"
               onMouseDown={() => moveToBottom(courseIndex)}
             >
+              <img src={downArrow} alt="moveUp" className="mr-2" />
               Move To Bottom
             </a>
             <a
               href="#"
-              className="block px-4 py-2 text-sm bg-white text-red-700 hover:bg-red-100"
+              className="block px-4 py-2 text-sm bg-white text-red-700 hover:bg-red-100 flex items-center"
               onMouseDown={() => removeItem(courseIndex)}
             >
+              <img src={deleteImg} alt="delete" className="mr-2" />
               Remove
             </a>
           </div>
